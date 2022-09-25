91483fff3855779397192e4b37d325d4c72a0451
lin-bo <imbo.lin@mail.utoronto.ca>
2022-09-24 21:41:09 -0400

    add solve trt map

---
+++ solve_trt_opt_map.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Author: Bo Lin
+
+import pandas as pd
+from utils.functions import load_file, str2list
+import geopandas as gpd
+import argparse
+
+
+def extract_projects(proj2art, projects):
+    arts = []
+    for p in projects:
+        arts += proj2art[p]
+    return arts
+
+
+def gen_proj_shp(n, sn, budget, potential):
+    filename = 'efficiency-n{}_id{}'.format(n, sn)
+    budget_col = 'budget_proj'
+    project_col = 'projects'
+    df = pd.read_csv('./prob/trt/res/{}/{}.csv'.format(potential, filename))
+    projects = df[df[budget_col] <= budget][project_col].values[-1]
+    print('Projects: ', projects)
+    df_art = gpd.read_file('./data/trt_arterial/trt_arterial.shp')
+    proj2artidx, _ = load_file('./data/trt_instance/proj2artid.pkl')
+    art_index = extract_projects(proj2artidx, str2list(projects))
+    df_selected = df_art.loc[art_index, :].copy()
+    df_selected.to_file(driver='ESRI Shapefile', filename='./prob/trt/res/shp/{}-budget{}.shp'.format(filename, budget))
+
+
+if __name__ == '__main__':
+    # parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--sns', nargs='+', type=int, help='series number of the p-median samples in a list')
+    parser.add_argument('-b', '--budget', type=int, help='budget (km * 4)')
+    parser.add_argument('--n', type=int, help='number of od pairs in the sample')
+    parser.add_argument('--potential', type=str, help='the potential for accessibility calculation, job/populations')
+    args = parser.parse_args()
+    # generate shapefile
+    for sn in args.sns:
+        gen_proj_shp(n=args.n, sn=args.sn, budget=args.budget, potential=args.potential)
--- solve_trt_opt_res.py
+++ solve_trt_opt_res.py
@@ -1,19 +1,16 @@
-import numpy as np
+#!/usr/bin/env python
+# coding: utf-8
+# Author: Bo Lin
+
 import pandas as pd
-from utils.functions import load_file, dump_file, str2list, cal_acc, gen_betas, des2od
+from utils.functions import load_file, str2list, cal_acc
 import os
-import geopandas as gpd
+import argparse
 
 
-def sol_summary(potential='job', method='greedy', n=500, sn=10, regu=0.):
+def sol_summary(potential='job',  n=500, sn=10):
     # load data
-    if method == 'greedy':
-        filename = 'greedy_abs_{}_par'.format(potential)
-        # res = load_file('./prob/trt/res/{}/{}.pkl'.format(potential, filename))
-    else:
-        filename = 'efficiency-n{}_id{}'.format(n, sn)
-    if regu > 0:
-        filename += '_regu{}'.format(regu)
+    filename = 'efficiency-n{}_id{}'.format(n, sn)
     if os.path.exists('./prob/trt/res/{}/{}.csv'.format(potential, filename)):
         res = pd.read_csv('./prob/trt/res/{}/{}.csv'.format(potential, filename))
     else:
@@ -26,11 +23,7 @@ def sol_summary(potential='job', method='greedy', n=500, sn=10, regu=0.):
     else:
         records, calculated = [], {}
     args, _ = load_file('./prob/trt/args.pkl')
-    if method == 'greedy':
-        budgets = np.arange(40, 401, 40)
-        projs = [res[res['allocated'] <= b]['selected'].values[-1] for b in budgets]
-    else:
-        projs, budgets = res['projects'].values, res['budget_proj'].values
+    projs, budgets = res['projects'].values, res['budget_proj'].values
     for idx, p in enumerate(projs):
         if budgets[idx] in calculated:
             continue
@@ -41,41 +34,13 @@ def sol_summary(potential='job', method='greedy', n=500, sn=10, regu=0.):
         df.to_csv(sum_path, index=False)
 
 
-def extract_projects(proj2art, projects):
-    arts = []
-    for p in projects:
-        arts += proj2art[p]
-    return arts
-
-
-def gen_proj_shp(n, sn, budget, potential, greedy=False):
-    filename = 'efficiency-n{}_id{}'.format(n, sn) if not greedy else 'greedy_abs_{}_par'.format(potential)
-    budget_col = 'budget_proj' if not greedy else 'allocated'
-    project_col = 'projects' if not greedy else 'selected'
-    df = pd.read_csv('./prob/trt/res/{}/{}.csv'.format(potential, filename))
-    projects = df[df[budget_col] <= budget][project_col].values[-1]
-    print('Projects: ', projects)
-    df_art = gpd.read_file('./data/trt_arterial/trt_arterial.shp')
-    proj2artidx, _ = load_file('./data/trt_instance/proj2artid.pkl')
-    art_index = extract_projects(proj2artidx, str2list(projects))
-    df_selected = df_art.loc[art_index, :].copy()
-    df_selected.to_file(driver='ESRI Shapefile', filename='./prob/trt/res/shp/{}-budget{}.shp'.format(filename, budget))
-
-
-def project_convert(selected_projects):
-    args, _ = load_file('./prob/trt/args.pkl')
-    projects = args['projects']
-    edges = []
-    for p in selected_projects:
-        edges += projects[p]
-    args, _ = load_file('./prob/trt/args_wo_yonge.pkl')
-    projects = args['projects']
-    return [idx for idx, p in enumerate(projects) if len([1 for f, t in edges if (f, t) in p])]
-
-
 if __name__ == '__main__':
+    # parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--sns', nargs='+', type=int, help='series number of the p-median samples in a list')
+    parser.add_argument('--n', type=int, help='number of od pairs in the sample')
+    parser.add_argument('--potential', type=str, help='the potential for accessibility calculation, job/populations')
+    args = parser.parse_args()
     # summarize results
-    for sn in range(21, 42):
-        sol_summary(potential='job', method='opt', n=2000, sn=sn)
-    # generate shapefile
-    gen_proj_shp(n=2000, sn=34, budget=280, potential='job', greedy=False)
+    for sn in args.sns:
+        sol_summary(potential=args.potential, n=args.n, sn=sn)
