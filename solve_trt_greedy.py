839f82df4c5bff17b8d5cd71c059ddbb84162df1
Madeleine Bonsma-Fisher <madeleine.bonsma@gmail.com>
2023-01-05 16:28:01 -0500

    add region

--- solve_trt_greedy.py
+++ solve_trt_greedy.py
@@ -3,17 +3,18 @@ from utils.instance_generator import RealInstanceGenerator
 import argparse
 
 
-def solve_greedy(budget, metric):
+def solve_greedy(budget, metric, region):
     # load data
     Generator = RealInstanceGenerator()
-    args = Generator.generate()
+    args = Generator.generate(region=region)
     # initialize the solver
     solver = GreedySolverPar(metric=metric, n_workers=4)
-    solver.solve(args, budget)
+    solver.solve(args, budget, region)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', '--budget', type=int, help='road design budget')
+    parser.add_argument('--region', type=str, help='region name')
     args = parser.parse_args()
-    solve_greedy(args.budget, 'abs')
+    solve_greedy(args.budget, 'abs', args.region)
