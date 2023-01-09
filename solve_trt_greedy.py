ae0cd2706d61c2c82394e0973eb83fc8aaacab6d
Madeleine Bonsma-Fisher <madeleine.bonsma@gmail.com>
2023-01-09 10:57:40 -0500

    add potential to greedy solver

--- solve_trt_greedy.py
+++ solve_trt_greedy.py
@@ -3,18 +3,19 @@ from utils.instance_generator import RealInstanceGenerator
 import argparse
 
 
-def solve_greedy(budget, metric, region=None):
+def solve_greedy(budget, metric, potential='job', region=None):
     # load data
     Generator = RealInstanceGenerator()
     args = Generator.generate(region=region)
     # initialize the solver
-    solver = GreedySolverPar(metric=metric, n_workers=4, region=region)
+    solver = GreedySolverPar(metric=metric, n_workers=4, potential=potential, region=region)
     solver.solve(args, budget)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', '--budget', type=int, help='road design budget')
+    parser.add_argument('--potential', type=str, help='type of accessibility', default='job')
     parser.add_argument('--region', type=str, help='region name', default=None)
     args = parser.parse_args()
-    solve_greedy(args.budget, 'abs', args.region)
+    solve_greedy(args.budget, 'abs', args.potential, args.region)
